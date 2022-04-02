58c7a8dc64a997e79d8487a0564040ce3b1eed60
Georgy <georgy.tertychniy@yandex.ru>
2022-04-02 10:43:31 +0300

    Entity base

---
+++ LudumDare/Assets/Entity/Entity.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour, IDamageable
+{
+    [SerializeField] private int _hp;
+    [SerializeField] private int _speed;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        
+    }
+}
