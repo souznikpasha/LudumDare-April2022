f6a877a0483996302f64123cc5444e015bb21ec6
Georgy <georgy.tertychniy@yandex.ru>
2022-04-02 11:42:11 +0300

    layers and player phys

--- LudumDare/Assets/Entity/Player.cs
+++ LudumDare/Assets/Entity/Player.cs
@@ -1,17 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : Entity
 {
-    void Start()
+    public static Player Instance;
+    public Controlls Controll;
+
+    private float _horizontalDir;
+    private void Awake()
     {
+        if (Instance == null)
+            Instance = this;
+        else
+        {
+            Destroy(this);
+        }
+
+        Controll = new Controlls();
+        Controll.Human.Enable();
+        Controll.Werewolf.Disable();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        Controll.Human.Move.performed += ctx => GetHorizontalDir(ctx.ReadValue<float>());
+        Controll.Human.Move.canceled += ctx => GetHorizontalDir(0);
+    }
+
+    protected void OnDisable()
+    {
+        Controll.Human.Move.performed -= ctx => GetHorizontalDir(ctx.ReadValue<float>());
+        Controll.Human.Move.canceled -= _ => GetHorizontalDir(0);
+    }
+    
+
+    void GetHorizontalDir(float dir)
+    {
+        _horizontalDir = dir;
+    }
+    private void ChangeState()
+   {
+       if(Controll.Werewolf.enabled)
+           Controll.Werewolf.Disable();
+       else
+           Controll.Werewolf.Enable();
+   }
+
+   void Update()
+    {
+        Move(_horizontalDir);
     }
 }
