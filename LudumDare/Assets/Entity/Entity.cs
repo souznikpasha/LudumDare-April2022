c62ec39355b02209f515125d2161247de96e9249
Georgy <georgy.tertychniy@yandex.ru>
2022-04-02 11:10:07 +0300

    entity

--- LudumDare/Assets/Entity/Entity.cs
+++ LudumDare/Assets/Entity/Entity.cs
@@ -4,21 +4,45 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour, IDamageable
 {
-    [SerializeField] private int _hp;
-    [SerializeField] private int _speed;
-    
-    void Start()
+    [SerializeField] protected int hp;
+    [SerializeField] protected float speed;
+    [SerializeField] protected float jumpForce;
+
+    protected Rigidbody2D rb;
+    protected Animator animator;
+    protected void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     
     void Update()
     {
         
     }
+    protected virtual void Move(float x)
+    {
+        rb.velocity = new Vector2(x * speed, rb.velocity.y);
 
+    }
+
+    protected virtual void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+    
     public void TakeDamage(int damage)
     {
-        
+        if (hp > 0)
+        {
+            hp -= damage;
+            if(hp<=0) 
+                Die();
+        }
+    }
+
+    void Die()
+    {
+        Destroy(this);
     }
 }
